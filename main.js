78a6c05c6f148e95cf50c817045cb79503cea41f
Sharon Byegon <byegon.sharon@gmail.com>
2024-04-05 01:08:33 +0300

    updated

--- main.js
+++ main.js
@@ -2,9 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Select all the hearts
+const articleHearts = document.getElementsByClassName('like-glyph');
 
+//function that handles the click event on the hearts
+function likeClick(event) {
+  // get the heart element that was clicked
+  const heart = event.target;
 
+  // mimic a server call
+  mimicServerCall("http://mimicServer.example.com") 
+    .then(() => {
+      // check if the heart's inner text is the empty heart
+      if (heart.innerText === EMPTY_HEART) {
+        // if it is, fill it with the full heart
+        heart.innerText = FULL_HEART;
+        heart.classList.add('activated-heart');
+      } else {
+        //otherwise, change the inner text to the empty heart
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch((error) => {
+      //if there's an error, show a modal with the error message 
+      const modal = document.getElementById('modal');
+      modal.classList.remove('hidden');
+      const modalMessage = document.getElementById('modal-message');
+      modalMessage.innerText = error;
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+//loop through all the article hearts and add a click event listener to each one
+for (let i=0; i<articleHearts.length; i++) {
+  articleHearts[i].addEventListener('click', likeClick);
+}
 
 
 //------------------------------------------------------------------------------
